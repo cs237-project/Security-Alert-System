c8b7ca203e4ee0f88d8df64d65b6b99c61bd32d5
FuyaoLi2017 <1605419584@qq.com>
2019-05-29 00:16:01 -0700

    update message chart

--- src/main/resources/static/js/rabbitmq.js
+++ src/main/resources/static/js/rabbitmq.js
@@ -15,7 +15,12 @@ $(document).ready(function () {
                 dataType: "json",
                 success: function (response) {
                     $("#createRabbitMQMessage").text(response.message);
+                    let data = response.data;
+                    sessionStorage.setItem("latitude", data[0]);
+                    sessionStorage.setItem("longitude", data[1]);
                     console.log("succesfully added clients");
+                    console.log("latitude:" + data[0]);
+                    console.log("longitude:" + data[1]);
                 },
                 error: function () {
                     $("#createRabbitMQMessage").text("something went wrong when creating messages");
@@ -72,8 +77,9 @@ $(document).ready(function () {
             dataType: "json",
             success: function (response) {
                 $("#getRabbitMQMessage").text(response.message);
-                document.getElementById("message_table").style.display='block';
-                createMessageTable(response);
+                // document.getElementById("message_table").style.display='block';
+                // createMessageTable(response);
+                showMessageGraph(response);
                 console.log("succesfully get messages for rabbitMQ");
             },
             error: function () {
@@ -106,24 +112,168 @@ $(document).ready(function () {
 
 });
 
-function createMessageTable(response) {
-    console.log("handleResult: populating message table from resultData");
-    let messageTable = jQuery("#message_table_body");
-    let result=response.data;
+// function createMessageTable(response) {
+//     console.log("handleResult: populating message table from resultData");
+//     let messageTable = jQuery("#message_table_body");
+//     let result=response.data;
+//
+//     for (let i = 0; i < result.length; i++) {
+//         let rowHTML = "";
+//         rowHTML += "<tr>";
+//         rowHTML += "<th>" + result[i]["messageId"] + "</th>";
+//         rowHTML += "<th>" + result[i]["type"] + "</th>";
+//         rowHTML += "<th>" + result[i]["location"] + "</th>";
+//         rowHTML += "<th>" + result[i]["happenTime"] + "</th>";
+//         rowHTML += "<th>" + result[i]["receivedTime"] + "</th>";
+//         rowHTML += "</tr>";
+//         messageTable.append(rowHTML);
+//     }
+//     console.log("message table: ",messageTable);
+// }
 
-    for (let i = 0; i < result.length; i++) {
-        let rowHTML = "";
-        rowHTML += "<tr>";
-        rowHTML += "<th>" + result[i]["messageId"] + "</th>";
-        rowHTML += "<th>" + result[i]["type"] + "</th>";
-        rowHTML += "<th>" + result[i]["location"] + "</th>";
-        rowHTML += "<th>" + result[i]["happenTime"] + "</th>";
-        rowHTML += "<th>" + result[i]["receivedTime"] + "</th>";
-        rowHTML += "</tr>";
-        messageTable.append(rowHTML);
+function showMessageGraph(response) {
+    let high_array = [];
+    let medium_array = [];
+    let low_array = [];
+
+    let data = response.data;
+
+    let totalLen = data.length;
+
+    for (let i = 0; i < totalLen; i++) {
+        let message = data[i];
+        if (message.location === "Within 3 miles"){
+            high_array.push({id: message['messageId'], x: i, y: message['receivedTime']});
+        } else if (message.location === "3-10 miles away") {
+            medium_array.push({id: message['messageId'], x: i, y: message['receivedTime']});
+        } else if (message.location === "Further than 10 miles") {
+            low_array.push({id: message['messageId'], x: i, y: message['receivedTime']});
+        }
     }
-    console.log("message table: ",messageTable);
+
+    console.log("high array:", high_array);
+    console.log("medium array:", medium_array);
+    console.log("low array:", low_array);
+
+    Chart.defaults.global.elements.point.radius = 1;
+    Chart.defaults.global.elements.point.hitRadius = 20;
+    Chart.defaults.global.elements.point.hoverBorderWidth = 4;
+
+        let datasets = [];
+
+        let dataset1 = {
+            label: 'high priority messages',
+            data: high_array,
+            backgroundColor: 'rgba(255, 0, 0, 1)',
+            borderColor: 'rgba(254, 100, 35, 1)',
+            borderWidth: 1,
+            fill: false,
+        };
+
+        let dataset2 = {
+            label: 'medium priority messages',
+            data: medium_array,
+            backgroundColor: 'rgba(254, 162, 35, 0.35)',
+            borderColor: 'rgba(254, 162, 35, 1)',
+            borderWidth: 1,
+            fill: false,
+        };
+
+        let dataset3 = {
+            label: 'low priority messages',
+            data: low_array,
+            backgroundColor: 'rgba(75, 192, 192, 0.35)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            fill: false,
+        };
+
+        datasets.push(dataset1);
+        datasets.push(dataset2);
+        datasets.push(dataset3);
+
+        let ctx = document.getElementById("message_graph").getContext('2d');
+        let myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: "Messages with Three Priorities",
+                datasets: datasets,
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: type
+                },
+                scales: {
+                    yAxes: [{
+                        type: 'linear',
+                        position: 'bottom',
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Message Sending Time',
+                            fontSize: 16
+                        },
+                        gridLines: {
+                            display: true
+                        },
+                        ticks: {
+                            beginAtZero:false,
+                            fontSize: 14,
+                        }
+                    }],
+                    xAxes: [{
+                        type: 'linear',
+                        position: 'bottom',
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Messages by Sequence',
+                            fontSize: 16
+                        },
+                        gridLines: {
+                            display: true
+                        },
+                        ticks: {
+                            beginAtZero:false,
+                            fontSize: 14,
+                        }
+                    }]
+                },
+                tooltips: {
+                    displayColors: false,
+                    callbacks: {
+                        title: function(tooltipItems, data) {
+                            let index = tooltipItems[0].index;
+                            let datasetIndex = tooltipItems[0].datasetIndex;
+                            let dataset = data.datasets[datasetIndex];
+                            let datasetItem = dataset.data[index];
+
+                            let message = response.data[datasetItem.id];
+                            return "MessageId: " + message.messageId;
+                        },
+                        label: function(tooltipItems, data) {
+                            let output = "";
+
+                            let index = tooltipItems.index;
+                            let datasetIndex = tooltipItems.datasetIndex;
+                            let dataset = data.datasets[datasetIndex];
+                            let datasetItem = dataset.data[index];
+
+                            let message = response.data[datasetItem.id];
+
+                            output += "location:" + message.location + "\n | \n";
+                            output += "Received Time: " + message.receivedTime;
+                            return output;
+                        }
+                    }
+                }
+            }
+        });
 }
+
+
+
+
 
 function createResultTable(response) {
     console.log("handleResult: populating result table from resultData");
